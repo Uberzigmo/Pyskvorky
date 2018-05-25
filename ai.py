424c18e25914fbbbf5e9ae08172a9a070ed813ee
zpiskoro <zuzana.piskorova@fei.com>
2018-05-25 10:55:37 +0200

    Docstring k funkci tah.

--- ai.py
+++ ai.py
@@ -16,9 +16,9 @@ def tah_pocitace(pole):
 def tah(pole, pozice, symbol):
     """
     Vymeni znak na dane pozici v poli za symbol.
-    :param pole:
-    :param pozice:
-    :param symbol:
-    :return:
+    :param pole: (list) aktualni hraci pole
+    :param pozice: (int) kterou pozici zamenit
+    :param symbol:  (str) jakym znakem nahradit znak na pozici
+    :return: None
     """
     return pole[:pozice] + symbol + pole[pozice+1:]
