a01424ecb4cd70fa2fcdc5f96f717ee3763d56f3
Ondrej Hejna <ondrej.hejna@thermofisher.com>
2018-05-30 21:38:17 +0200

    Zprovozneni areny a zapisovani vysledku

--- piskvorky_1d.py
+++ piskvorky_1d.py
@@ -3,45 +3,51 @@ from ai_1 import tah
 from ai_2 import tah_pocitace
 
 def vyhodnot(pole): #vyhodnocuje pole!
+
+    """
+    Vyhleda v poli podminky pro vyhru hrace A nebo B nebo pripadnou remizu
+    Pokud vraci False, tak hra pokracuje
+    :param pole: (str) pole, kde to zije!
+    :return: (str) vrati vyhru, prohru, remizu nebo Velky Spatny
+    """
     if "xxx" in pole:
-        print('Vyhral x')
-        return 'x'
+        #print('Vyhral x')
+        return "x"
     elif 'ooo' in pole:
-        print('Vyhral o')
-        return 'o'
+        #print('Vyhral o')
+        return "o"
+    elif "-" not in pole:
+        #print("Remiza")
+        return "."
     else:
         return False
 
 
-def tah_hrace(pole):
-    symbol = 'x'
-    try:
-        pozice = int(input('Na jakou pozici chces hrat?: '))
-    except ValueError:
-        return tah_hrace(pole)
-    if pole[pozice] == '-':
-        return tah(pole, pozice, symbol)
-    else:
-        print('Na tuhle pozici hrat nelze.')
-        return tah_hrace(pole)
-
-
-
 def piskvorky1D(strategie1, strategie2, delka_pole=20):
+    """
+    Hra Valka svetu (resp. x vs o)
+    :param strategie1: nacteni strategie prvniho hrace, vraci pole s zahranym symbolem
+    :param strategie2: nacteni strategie druheho hrace, vraci pole s zahranym symbolem
+    :param delka_pole: delka 1D Pyskvorek
+    :return: vraci finalni stav po pyskvorovem masakru
+    """
+
     pole = delka_pole*'-'
-    stav = vyhodnot(pole)
+    stav = False
     while not stav:
         pole = strategie1(pole)
-        print(pole)
-        stav = vyhodnot(pole)
-
+        if vyhodnot(pole) == "o":
+            print("Vyhrava 'o':   {}".format(pole))
+            break
         pole = strategie2(pole)
-        print(pole)
-        stav = vyhodnot(pole)
+        if vyhodnot(pole) == "x":
+            print("Vyhrava 'x':   {}".format(pole))
+            break
+        if vyhodnot(pole) == ".":
+            print("Remiza:   {}".format(pole))
+            break
+    stav = vyhodnot(pole)
     return stav
-
-
-#print(piskvorky1D(tah_hrace, tah_pocitace))
 
 
 
